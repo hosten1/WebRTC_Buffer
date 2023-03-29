a7474c79a424132c9982a8669d18fb0dc1c23c34
luoyongmeng <hosten_lym@163.com>
2023-03-29 08:49:52 +0800

    Update testStun.cc

--- testBuffer/testStun.cc
+++ testBuffer/testStun.cc
@@ -1069,34 +1069,47 @@ namespace rtc {
           reinterpret_cast<const char*>(buf1.Data()), buf1.Length()));
     }
 void test_sendBindMsg(){
+    
+    //1. 构建 stun 消息
     StunMessage request;
     request.SetType(STUN_BINDING_REQUEST);
     std::string str =   reinterpret_cast<const char*>(kRfc5769SampleMsgTransactionId);
     request.SetTransactionID(str);
     /*
+     可以使用这个代码验证
   //           MD5加密文本：lym:example.org:123456
   //           turnKey = 8812c1afb0e203aae88c996e30ac7db6
   //           unsigned char  data[7] = "123456";
   //           hmac_sha1加密后数据 = b103f699ef12c04ab6f0cb155ac2f12ef84adf22
   //           */
     std::string key;
+    // 计算MD5, 也就是HMAC的key
     ComputeStunCredentialHash("lym","example.org","123456", &key);
-    request.AddMessageIntegrity(key);
-    const StunByteStringAttribute* mi_attr =
-    request.GetByteString(STUN_ATTR_MESSAGE_INTEGRITY);
-    printfX("StunTest_AddMessageIntegrity MD5 ",mi_attr->bytes(), mi_attr->length());
+  
     
     request.AddAttribute(std::make_unique<StunByteStringAttribute>(STUN_ATTR_USERNAME, "username"));
     request.AddAttribute(std::make_unique<StunUInt32Attribute>(STUN_ATTR_RETRANSMIT_COUNT, 1));
     request.AddAttribute(std::make_unique<StunByteStringAttribute>(STUN_ATTR_SOFTWARE, "{\"name\":\"lym\",\"age\":10,\"body\":\"haha\"}"));
+    
+    // 使用 key计算 HMAC 并加入到buffer中；
+    request.AddMessageIntegrity(key);
+     // test  这里是验证代码
+    const StunByteStringAttribute* mi_attr =
+    request.GetByteString(STUN_ATTR_MESSAGE_INTEGRITY);
+    printfX("StunTest_AddMessageIntegrity MD5 ",mi_attr->bytes(), mi_attr->length());
+    
+    
+    // 转成二进制
     rtc::ByteBufferWriter writer;
     request.Write(&writer);
     printfX("test_sendBindMsg ",writer.Data() , writer.Length());
     
     
+    // 2. 解析 stun 消息
     StunMessage msg;
     rtc::ByteBufferReader buf(writer.Data() , writer.Length());
     msg.Read(&buf);
+   // 获取解析后的消息
     const StunByteStringAttribute *soft_attr = msg.GetByteString(STUN_ATTR_SOFTWARE);
     std::string soft_attrStr = mi_attr->GetString();
     printf("test_sendBindMsg soft_attr %s %zu",soft_attrStr.c_str(), mi_attr->length());
